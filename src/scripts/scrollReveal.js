1076cfce93c78daad30c7d978e8609e6d2e7828a
root <root@Simogoogoo>
2024-06-01 19:04:12 +0200

    Starting to do simple folio

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -7,3 +7,23 @@ export default function initScrollReveal(targetElements, defaultProps) {
     ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const framedElements = document.querySelectorAll('.framed');
+  const observerOptions = {
+    root: null, // Utilise le viewport comme conteneur
+    rootMargin: '0px',
+    threshold: 0.1 // Déclenche l'animation quand 10% de l'élément est visible
+  };
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target); // Arrête d'observer une fois l'animation déclenchée
+      }
+    });
+  }, observerOptions);
+  framedElements.forEach(element => {
+    observer.observe(element);
+  });
+});
